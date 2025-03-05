c8ab8f4f37ee8640f104d5534557f5a29638dadf
alameen-47 <alameenkhan1431@gmail.com>
2025-03-06 00:03:51 +0300

    +

--- frontend/screens/HomeScreen.jsx
+++ frontend/screens/HomeScreen.jsx
@@ -4,6 +4,7 @@ import {
   SafeAreaView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
 import {
@@ -15,14 +16,17 @@ export const HomeScreen = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <ImageBackground
-        className="h-full"
+        className="h-screen"
         source={require('../assets/main.jpg')}>
-        <View className="h-screen m-auto flex align-middle justify">
+        <View className="flex align-end align-middle h-full ">
           <Text
-            style={[{fontSize: wp(22)}, styles.text]}
-            className="text-[#ffffff88] rotate-90 ">
+            style={[{fontSize: wp(23)}, styles.text]}
+            className="text-[#ffffff88] rotate-90 left-[40%] m-auto ">
             FITNESS
           </Text>
+          <TouchableOpacity className="bg-white p-3 bottom-[10%] rounded-lg w-[50%] flex justify-center align-middle items-center mx-[25%]">
+            <Text style={[{fontSize: wp(6)}, styles.text]}>Get Started</Text>
+          </TouchableOpacity>
         </View>
       </ImageBackground>
     </SafeAreaView>
