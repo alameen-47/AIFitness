a43f500f8d392c0afa07f72813d01197162ecc61
alameen-47 <alameenkhan1431@gmail.com>
2025-03-12 00:02:23 +0300

    CAmera added

--- frontend/screens/trainer/CameraScreen.jsx
+++ frontend/screens/trainer/CameraScreen.jsx
@@ -1,54 +1,44 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {Camera, useCameraDevices} from 'react-native-vision-camera';
 
 export const CameraScreen = () => {
   const navigation = useNavigation();
-  const [hasPermission, setHasPermission] = useState(null);
   const devices = useCameraDevices();
-  console.log('Available devices:', devices);
-
-  const device = devices.back ?? devices.front;
-
+  const device = devices.find(d => d.position === 'back');
+  // const device = device[0];
+  console.log(devices, 'DEVICESSSSS@#@@@@');
   useEffect(() => {
-    setTimeout(() => {
-      const status = Camera.getCameraPermissionStatus();
-      setHasPermission(status === 'granted');
-    }, 2000);
+    checkPermission();
   }, []);
-  
-
-  useEffect(() => {
-    (async () => {
-      const cameraPermission = await Camera.requestCameraPermission();
-      // const microphonePermission = await Camera.requestMicrophonePermission();
-
-      if (cameraPermission === 'granted') {
-        setHasPermission(true);
-      } else {
-        setHasPermission(false);
-      }
-    })();
-  }, []);
-  if (hasPermission === null) {
-    return <Text>Requesting Camera Permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>Permission Denied</Text>;
-  }
-  if (!device) {
-    return <Text>No camera device found</Text>
-    ;
-  }
+  const checkPermission = async () => {
+    const newCameraPermission = await Camera.requestCameraPermission();
+    const newMicrophonePermission = await Camera.reequestMicrophonePermission();
+    console.log(newCameraPermission);
+  };
+  if (device == null) return <ActivityIndicator />;
   return (
-    <View className="bg-green-500 h-screen">
-      <Camera device={device} isActive={true} className="flex" />
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Text>Close CAmera</Text>
+    <View className="bg-green-500 h-screen w-screen ">
+      <Camera
+        device={device}
+        isActive={true}
+        className="flex h-screen w-screen"
+        style={StyleSheet.absoluteFill}
+      />
+      <TouchableOpacity
+        className="bg-yellow-400 border-1 border-black flex text center"
+        onPress={() => navigation.goBack()}>
+        <Text className="font-bold">Close CAmera</Text>
       </TouchableOpacity>
 
-      <Text>CameraScreen</Text>
+      <Text className="bg-green-400 ">CameraScreen</Text>
     </View>
   );
 };
